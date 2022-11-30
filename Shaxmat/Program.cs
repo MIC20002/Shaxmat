4f5c1bbb62abd046a3a5b4e0d8ba9ccfe05e2ad7
Qaramyan V <vazgen_qaramyan@mail.ru>
2022-11-30 08:59:36 -0800

    Add project files.

---
+++ Shaxmat/Program.cs
@@ -0,0 +1,69 @@
+﻿int[,] myMAtrix;
+myMAtrix = new int[8, 8];
+Console.WriteLine("D or N");
+string DN = Console.ReadLine();
+
+
+    Console.WriteLine("Type x");
+    int x = int.Parse(Console.ReadLine());
+    Console.WriteLine("Type y");
+    int y = int.Parse(Console.ReadLine());
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            myMAtrix[i, j] = 0;
+
+        }
+    }
+if (DN == "D")
+{
+    myMAtrix[x, y] = 1;
+    myMAtrix[x + 2, y + 1] = 2;
+    myMAtrix[x + 1, y + 2] = 3;
+    myMAtrix[x - 1, y + 2] = 4;
+    myMAtrix[x - 2, y - 1] = 5;
+    myMAtrix[x - 2, y - 1] = 6;
+    myMAtrix[x - 1, y - 2] = 7;
+    myMAtrix[x + 1, y - 2] = 8;
+    myMAtrix[x + 2, y - 1] = 9;
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            Console.Write(myMAtrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+else
+{ 
+    for (int i = 0; i < 8; i++)
+    {
+        for (int j = 0; j < 8; j++)
+        {
+            myMAtrix[i, j] = 0;
+
+        }
+        //myMAtrix[x, y] = 1;
+        //myMAtrix[x + 2, y + 1] = 2;
+        //myMAtrix[x + 1, y + 2] = 3;
+        //myMAtrix[x - 1, y + 2] = 4;
+        //myMAtrix[x - 2, y - 1] = 5;
+        //myMAtrix[x - 2, y - 1] = 6;
+        //myMAtrix[x - 1, y - 2] = 7;
+        //myMAtrix[x + 1, y - 2] = 8;
+        //myMAtrix[x + 2, y - 1] = 9;
+    }
+ }
+for (int i = 0; i < 8; i++)
+{
+    for (int j = 0; j < 8; j++)
+    {
+        Console.Write(myMAtrix[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+
+
